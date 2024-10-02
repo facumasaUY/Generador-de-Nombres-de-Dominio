2c63c2b25bb37d8c5e7907d0f1ce57e471340b74
facumasaUY <facundomasa@gmail.com>
2024-10-02 01:52:27 +0000

    im not sure yet

--- src/app.js
+++ src/app.js
@@ -7,5 +7,21 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  console.log(`${pronoun[p]} ${adj[a]} ${noun[n]} ${extensions[e]}`);
 };
+
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let extensions = [".edu", ".br", ".us", ".org", ".com", ".net", ".ar", ".uy"];
+
+for (let p = 0; p < pronoun.length; p++) {
+  for (let a = 0; a < adj.length; a++) {
+    for (let n = 0; n < noun.length; n++) {
+      for (let e = 0; e < extensions.length; e++) {
+        console.log(`${pronoun[p]} ${adj[a]} ${noun[n]} ${extensions[e]}`);
+      }
+    }
+  }
+}
